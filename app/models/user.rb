21581d9a0bcbdd5c3bee981739c7fc7227d2acd6
DeeAnn Kendrick <deeannkcooper@bellsouth.net>
2016-11-28 11:56:22 -0500

    include secure passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
   #parens and curlys are optional
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
+  validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
 end
