d049d8de1a48387be29de4b9225a25d102dc8099
DeeAnn Kendrick <deeannkcooper@bellsouth.net>
2016-12-07 12:49:54 -0500

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -19,5 +28,27 @@ class User < ApplicationRecord
   # Defines a proto-feed.
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  #follows a user
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+    #can also be
+    #user.active_relationships.create(followed_id: other_user.id)
+  end
+  #unfollows a user
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+  #Returns true if the current user is following the other user
+  def following?(other_user)
+    following.include?(other_user)
+  end
+  # Returns a user's status feed.
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -73,4 +73,33 @@ class UserTest < ActiveSupport::TestCase
       @user.destroy
     end
   end
+
+  test "should follow and unfollow a user" do
+    dee = users(:dee)
+    archer  = users(:archer)
+    assert_not dee.following?(archer)
+    dee.follow(archer)
+    assert dee.following?(archer)
+    assert archer.followers.include?(dee)
+    dee.unfollow(archer)
+    assert_not dee.following?(archer)
+  end
+
+  test "feed should have the right posts" do
+    michael = users(:dee)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    # Posts from self
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
 end
